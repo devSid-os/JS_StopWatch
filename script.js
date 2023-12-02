fd04372e21e6cd557ab7485877f9eb98b3240f2e
devSid-os <siddjain341@gmail.com>
2023-12-02 16:03:27 +0530

    TimeLapse Functionality implemented correctly

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var timeObject = {
 };
 var playInterval;
 var timeLapseIndex = 0;
-var differenceTimeObject = {
+var timeStamps = {
     miliSeconds: 0,
     seconds: 0,
     minutes: 0,
@@ -95,27 +95,44 @@ resetButton.addEventListener("click", function () {
     playButton.classList.add("fa", "fa-play");
     hoursHtml.classList.add("hidden");
     hoursHtml.nextElementSibling.classList.add("hidden");
+    while (timeLapseTable.firstChild) timeLapseTable.removeChild(timeLapseTable.firstChild);
+    timeLapseTable.classList.add("hidden");
+    timeLapseIndex = 0;
+    differenceTimeObject.miliSeconds = 0;
+    differenceTimeObject.seconds = 0;
+    differenceTimeObject.minutes = 0;
+    timeObject.miliSeconds = 0;
+    timeObject.seconds = 0;
+    timeObject.minutes = 0;
 });
 
 function setValue(value) {
     if (value < 10) return `0${value}`;
-    return value;
+    return value.toString();
 }
 
 timeLapseButton.addEventListener("click", function () {
     var includeHours = hoursHtml.classList.contains("hidden");
     var currentTime = !includeHours ? setValue(timeObject.hours) + " : " + setValue(timeObject.minutes) + " : " + setValue(timeObject.seconds) + " : " + setValue(timeObject.miliSeconds) : setValue(timeObject.minutes) + " : " + setValue(timeObject.seconds) + " : " + setValue(timeObject.miliSeconds);
     ++timeLapseIndex;
-    differenceTimeObject.miliSeconds = Math.abs(timeObject.miliSeconds - differenceTimeObject.miliSeconds);
-    differenceTimeObject.seconds = Math.abs(timeObject.seconds - differenceTimeObject.seconds);
-    differenceTimeObject.minutes = Math.abs(timeObject.minutes - differenceTimeObject.minutes);
     timeLapseTable.classList.remove("hidden");
     const tr = document.createElement("tr");
     const td1 = document.createElement("td");
     const td2 = document.createElement("td");
     const td3 = document.createElement("td");
     td1.textContent = timeLapseIndex < 10 ? "0" + timeLapseIndex : timeLapseIndex;
-    td2.textContent = setValue(differenceTimeObject.minutes) + " : " + setValue(differenceTimeObject.seconds) + " : " + setValue(differenceTimeObject.miliSeconds);
+    if (timeLapseIndex === 1) {
+        timeStamps.miliSeconds = parseInt(miliSecondsHtml.textContent);
+        timeStamps.seconds = parseInt(secondsHtml.textContent);
+        timeStamps.minutes = parseInt(minutesHtml.textContent);
+        td2.textContent = "+ " + setValue(timeObject.minutes) + " : " + setValue(timeObject.seconds) + " : " + setValue(timeObject.miliSeconds);
+    }
+    else {
+        td2.textContent = "+ " + setValue(Math.abs(parseInt(minutesHtml.textContent) - timeStamps.minutes)) + " : " + setValue(Math.abs(parseInt(secondsHtml.textContent) - timeStamps.seconds)) + " : " + setValue(Math.abs(parseInt(miliSecondsHtml.textContent) - timeStamps.miliSeconds));
+        timeStamps.minutes = parseInt(minutesHtml.textContent);
+        timeStamps.seconds = parseInt(secondsHtml.textContent);
+        timeStamps.miliSeconds = parseInt(miliSecondsHtml.textContent);
+    }
     td3.textContent = currentTime;
     td1.classList.add("border", "border-slate-500", "text-center");
     td2.classList.add("border", "border-slate-500", "text-center");
@@ -123,5 +140,5 @@ timeLapseButton.addEventListener("click", function () {
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
-    timeLapseTable.childNodes[1].prepend(tr);
+    timeLapseTable.prepend(tr);
 });
